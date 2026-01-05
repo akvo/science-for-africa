a76088ecfb5f3f5e57209f62b38767f0879be742
zuhdil <zuhdil@gmail.com>
2026-01-05 13:51:42 +0700

    Make production build working

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -1,10 +1,15 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('BACKEND_URL', '/cms'),
   app: {
     keys: env.array('APP_KEYS'),
   },
+  // Trust proxy headers from nginx
+  proxy: {
+    koa: true,
+  },
+  // Public URL for Strapi (webhooks, emails, OAuth)
+  url: env('BACKEND_URL', 'http://localhost/cms'),
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },

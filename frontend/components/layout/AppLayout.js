c822bf0705be0ad25f4a5b4ee6533d11319a7e69
Navin Shingote <nikshingote@gmail.com>
2026-04-09 21:22:31 +0530

    [#18] - Implement static version of collaboration calls page (#14)

--- frontend/components/layout/AppLayout.js
+++ frontend/components/layout/AppLayout.js
@@ -3,14 +3,14 @@ import Navbar from "./Navbar";
 
 const AppLayout = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col bg-white overflow-x-hidden">
+    <div className="min-h-screen flex flex-col bg-white overflow-x-clip">
       <Navbar />
       {/*
         Spacing to account for fixed navbar:
         HeaderTop (34px) + MainNav (80px) = 114px
       */}
       <main className="grow pt-28.5">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-8">
           {children}
         </div>
       </main>

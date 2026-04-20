ba6e06a4e2285ae347aaa74a82fb1c4d6fa01a8e
wayangalihpratama <wgprtm@gmail.com>
2026-04-20 13:08:38 +0800

    [#15] revert: restore original strapi session middleware configuration for troubleshooting

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -6,19 +6,7 @@ module.exports = [
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
-  {
-    name: "strapi::session",
-    config: {
-      key: "koa.sess",
-      maxAge: 86400000,
-      autoCommit: true,
-      overwrite: true,
-      httpOnly: true,
-      clearInvalid: true,
-      sameSite: "lax",
-      secure: false, // Set to false to avoid 'Cannot send secure cookie' crash behind Google LB/Nginx proxies that strip X-Forwarded-Proto
-    },
-  },
+  "strapi::session",
   "strapi::favicon",
   "strapi::public",
   "global::documentation-redirect",

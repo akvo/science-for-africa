5caaaf4d552f1c9c6a4a5beb68d5437866affd6b
wayangalihpratama <wgprtm@gmail.com>
2026-04-25 06:28:10 +0800

    [#15] fix(auth): fix secure session cookie error in production

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -6,7 +6,13 @@ module.exports = [
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
-  "strapi::session",
+  {
+    name: "strapi::session",
+    config: {
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+    },
+  },
   "strapi::favicon",
   "strapi::public",
   "global::documentation-redirect",

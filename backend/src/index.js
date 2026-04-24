9ddd9e52251f39bde0b1c1e56f9c1e1f4402debf
wayangalihpratama <wgprtm@gmail.com>
2026-04-25 06:28:18 +0800

    [#15] fix(auth): improve google oauth synchronization logic

--- backend/src/index.js
+++ backend/src/index.js
@@ -407,8 +407,9 @@ module.exports = {
         "http://localhost:3000"
       ).replace(/\/$/, "");
 
-      const backendUrlForLogs = (process.env.BACKEND_URL || "http://localhost:1337")
-        .replace(/\/$/, "");
+      const backendUrlForLogs = (
+        process.env.BACKEND_URL || "http://localhost:1337"
+      ).replace(/\/$/, "");
 
       // Use essentials only to prevent header bloat
       const googleConfig = {

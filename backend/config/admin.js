5f34026f81c4d16cad208485ba96ffe11292bfa6
wayangalihpratama <wgprtm@gmail.com>
2026-04-29 10:03:45 +0800

    [#56] fix(backend): set relative admin URL for correct subpath asset resolution

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -1,20 +1,18 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'test-secret'),
+    secret: env("ADMIN_JWT_SECRET", "test-secret"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT', 'test-api-token-salt'),
+    salt: env("API_TOKEN_SALT", "test-api-token-salt"),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT', 'test-transfer-token-salt'),
+      salt: env("TRANSFER_TOKEN_SALT", "test-transfer-token-salt"),
     },
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: env.bool("FLAG_NPS", true),
+    promoteEE: env.bool("FLAG_PROMOTE_EE", true),
   },
-  watchIgnoreFiles: [
-    '**/config/sync/**',
-  ],
+  watchIgnoreFiles: ["**/config/sync/**"],
 });

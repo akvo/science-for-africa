b277b37deca57be5d5a48eb1a1f1c508db847c58
wayangalihpratama <wgprtm@gmail.com>
2026-05-12 14:13:06 +0800

    [#87] fix(i18n): update Footer, 404, and index components for standardized keys

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -425,3 +425,11 @@ export default function StyleGuide() {
     </div>
   );
 }
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common", "profile"])),
+    },
+  };
+}

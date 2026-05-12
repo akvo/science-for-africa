3584d9c91fe6f21b67514e6fddc008fb618e1870
wayangalihpratama <wgprtm@gmail.com>
2026-05-12 14:15:52 +0800

    [#87] style(index): move getStaticProps and expand translation namespaces

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -425,11 +425,3 @@ export default function StyleGuide() {
     </div>
   );
 }
-
-export async function getStaticProps({ locale }) {
-  return {
-    props: {
-      ...(await serverSideTranslations(locale, ["common", "profile"])),
-    },
-  };
-}
